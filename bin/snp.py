515e40fcce852180b948c3ad39080d6f90b41130
LYC-vio <49940906+LYC-vio@users.noreply.github.com>
2019-09-18 17:15:39 -0700

    Update snp.py

--- bin/snp.py
+++ bin/snp.py
@@ -8,7 +8,10 @@ def GetGoldSNP(gzpath):
             if line[0] != '#':
                 line = line.split('\t')
                 TYPE = line[9].split(":")[0]
-                TYPE = int(TYPE[0])*int(TYPE[2])
+                if "." in TYPE:
+                    TYPE = 0
+                else:
+                    TYPE = int(TYPE[0])*int(TYPE[2])
                 if TYPE ==2:
                     alt1,alt2 = line[4].split(",")
                     CHROM = line[0]
